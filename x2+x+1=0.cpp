cf90d0c0c7925e5ed1addb63016e049add7e4f61
liteni7002-dotcom <liteni7002@gmail.com>
2025-09-29 17:20:32 +0700

    дз4

--- x2+x+1=0.cpp
+++ x2+x+1=0.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 #include <cmath>
 
-int main(){
-	float a, b, c; 
+int main() {
+	float a, b, c;
 	std::cout << "enter coefficients";
 	std::cin >> a >> b >> c;
 	if (a == 0) {
-		if (b == 0){
-			std::cout << "There is no real roots";
+		if (b == 0) {
+			if (c == 0) {
+				std::cout << "This equation has an infinite number of roots.";
+			}
+			else {
+				std::cout << "Error: incorrect equation!";
+			}
 		}
 		else {
 			float x = c / b;
-			std::cout << "x="<<x;
+			std::cout << "This equation has one real root: x=" << x;
 		}
 	}
 	else {
-		float d = (pow(b,2) - 4 * a * c);
+		float d = (pow(b, 2) - 4 * a * c);
 		if (d < 0) {
 			std::cout << "There is no real roots";
 		}
@@ -23,10 +28,10 @@ int main(){
 			float x1 = (-b + pow(d, 0.5)) / (2 * a);
 			float x2 = (-b - pow(d, 0.5)) / (2 * a);
 			if (x1 == x2) {
-				std::cout << "x=" << x1;
+				std::cout << "This equation has two equal real roots: x1,2=" << x1;
 			}
 			else {
-				std::cout << "x1=" << x1<<"\n";
+				std::cout << "This equation has two real roots: x1=" << x1 << "\n";
 				std::cout << "x2=" << x2;
 			}
 		}
